59d3ba679ba4eb2c6d00a32130ad952bf65cefdb
ken <keanolida7296@gmail.com>
2022-06-08 15:22:05 +0800

    done for lesson 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 // import logo from './logo.svg';
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
-
 export class App extends Component {
-  render() {
-    return (
-      <div className=''>
-        <Navbar dark color="primary">
-            <div className='container'>
-                <NavbarBrand href="/">Home</NavbarBrand>
-                <NavbarBrand href="/about">About</NavbarBrand>
-                <NavbarBrand href="/contact">Contact</NavbarBrand>
-            </div>
-        </Navbar>
-      </div>
-    )
-  }
+    render() {
+        return (
+        <div className=''>
+            <Navbar dark color="success">
+                <div className='container'>
+                    <NavbarBrand href="/">Home</NavbarBrand>
+                    <NavbarBrand href="/menu">Menu</NavbarBrand>
+                    <NavbarBrand href="/about">About</NavbarBrand>
+                    <NavbarBrand href="/contact">Contact</NavbarBrand>
+                </div>
+            </Navbar>
+            <Menu/>
+        </div>
+        )
+    }
 }
 
 // const App = React.createElement(
